56817913510434d91d413db350a5c88ae1fc3d2a
supakit25433 <supakrit.kit@mail.kmutt.ac.th>
2020-09-25 10:38:53 +0700

    add Route in Switch

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Login from './components/LoginForm';
 import Header from './components/Header';
 import About from './components/About'
 import Contact from './components/Contact';
+import Register from './components/RegisterForm'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
 
@@ -16,7 +17,7 @@ function App() {
           <Route path='/about' component={About} />
           <Route path='/contact' component={Contact} />
           <Route path='/login' component={Login} />
-          {/* <Route path='/signup' component={Register} /> */}
+          <Route path='/signup' component={Register} />
         </Switch>
       </div>
     </Router>
